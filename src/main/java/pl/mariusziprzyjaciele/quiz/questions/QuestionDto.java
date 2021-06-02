b5d3cb32c152bf8dbaad155a58413ffd1991b149
Kamil Nowak <kamilnowakx98@gmail.com>
2021-06-02 08:52:52 +0200

    fix problems

--- src/main/java/pl/mariusziprzyjaciele/quiz/questions/QuestionDto.java
+++ src/main/java/pl/mariusziprzyjaciele/quiz/questions/QuestionDto.java
@@ -5,17 +5,19 @@ import lombok.Setter;
 import pl.mariusziprzyjaciele.quiz.answers.AnswerDto;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class QuestionDto {
     private String id;
     private String question;
-    private AnswerDto[] answerDtos;
+    private List<AnswerDto> answerDtos;
 
     public QuestionDto(Question question) {
         this.id = question.getId();
         this.question = question.getQuestion();
-        this.answerDtos = (AnswerDto[]) Arrays.stream(question.getAnswers()).map(AnswerDto::new).toArray();
+        this.answerDtos = Arrays.stream(question.getAnswers()).map(AnswerDto::new).collect(Collectors.toList());
     }
 }
